28938a93fe7c6e7b5b590b998b3c29ad87005fe9
Adam Lash <adamlash@microsoft.com>
2020-09-28 12:11:26 -0400

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # aad-oauth-spa-with-apim
-Azure AD authenticated with Oauth to an SPA backed by APIs from APIM
+Azure AD authenticated with Oauth to an SPA backed by APIs from APIM. Two Examples
+
+1. Implicit flows with Oauth
+2. Authorization flows with Oauth
+
+The main differential will be the SPA itself and how it interacts with APIM (at least that's the theory...)
+
+
+## Goals
+To show a Single Page Application authenticated with AAD using Oauth 2.0 and OIDC, with that same authenticaiton used to authorize relevant backend services.
+
+## Tech Used/Displayed in this demo
+1. APIs build on Azure Function Apps
+2. APIs served from APIM
+3. WebApp hosting SPA
+4. Identities registered in App Registrations
+
+## API Design
+Basic API from https://docs.microsoft.com/en-us/azure/api-management/howto-protect-backend-frontend-azure-ad-b2c example hosted on Function App
+
+APIM Configured
+
+
+## APP Registrations
+
+## SPA Code
---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = { token: '', backend: '', backend2: '' };
+  }
+
+  async componentDidMount() {
+    try {
+      const request = await fetch('/.auth/me');
+      const response = await request.json();
+      const jsonString = JSON.stringify(response)
+      console.log('String: ' + jsonString);
+      const jsonArray = JSON.parse(jsonString)
+      console.log('Array: '+ jsonArray);
+      var id_token =jsonArray[0].id_token;  // or jsonArray[0].access_token
+      console.log('Token: '+ id_token);
+      this.setState({ token: jsonArray[0].id_token });
+      if (jsonArray !== undefined) {
+        console.log('Array has content');
+        const requestToBackEnd = await fetch('https://adamlashapim.azure-api.net/adamlash/helloWorld',
+        {
+          method: "GET",
+          //body: JSON.stringify(data),
+          headers: {
+            "Authorization": "Bearer " + id_token 
+          },
+          //credentials: "same-origin"
+        });
+        const responseFromBackEnd = await requestToBackEnd.json();
+        const jsonStringBackEnd = JSON.stringify(responseFromBackEnd)
+        this.setState({ backend: jsonStringBackEnd });
+        console.log('String Backend: ' + jsonStringBackEnd);
+
+        axios({
+          method: 'get',
+          url: 'https://adamlashapim.azure-api.net/adamlash/helloWorld',
+          headers: {'Authorization': "Bearer " + id_token, 'Accept' : 'application/json', 'Content-Type': 'application/json'},
+          cancelToken: new axios.CancelToken((token) => {
+          this.cancelToken = token;
+          })
+        })
+        .then((res) => {
+          this.cancelToken = null;
+          this.setState({ backend2: JSON.stringify(res.data) });
+        })
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          Token from Azure AD: { this.state.token }
+        </p>
+        <p className="App-intro">
+          Values from Backend: { this.state.backend }
+        </p>
+        <p className="App-intro">
+          Values from Backend using axios: { this.state.backend2 }
+        </p>
+      </div>
+    );
+  }
+
+}
+
+export default App;
